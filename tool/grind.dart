2d876bdea62ddd2e158ffb75a1b6f64793ea65e7
Brady Becker <brady.becker@webfilings.com>
2016-01-03 21:27:04 -0600

    adding coveralls to the build

---
+++ tool/grind.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:grinder/grinder.dart';
+
+void main(List<String> args) {
+  coverage();
+}
+
+@Task('Gather and send coverage data')
+void coverage() {
+  // coveralls doesn't seem to work for html tests
+  if (Platform.environment['COVERALLS_TOKEN'] != null) {
+    // report to coveralls
+    Pub.global.activate('dart_coveralls');
+    run('dart_coveralls', arguments: [
+      'report',
+      'test/all.dart'
+    ]);
+  } else {
+    // run coverage locally
+    Pub.global.run('dart_dev', arguments: ['coverage']);
+  }
+}
