e57b192decda328456de38c372fbfe9e00743a15
Brady Becker <brady.becker@webfilings.com>
2016-01-03 21:29:51 -0600

    formating...

--- tool/grind.dart
+++ tool/grind.dart
@@ -11,10 +11,7 @@ void coverage() {
   if (Platform.environment['COVERALLS_TOKEN'] != null) {
     // report to coveralls
     Pub.global.activate('dart_coveralls');
-    run('dart_coveralls', arguments: [
-      'report',
-      'test/all.dart'
-    ]);
+    run('dart_coveralls', arguments: ['report', 'test/all.dart']);
   } else {
     // run coverage locally
     Pub.global.run('dart_dev', arguments: ['coverage']);
